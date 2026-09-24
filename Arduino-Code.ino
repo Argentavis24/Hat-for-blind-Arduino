a73e0e3b9a42c99634488659897d12d4cd764b0b
Argentavis <krithickm24@gmail.com>
2026-09-24 20:41:04 +0530

    Refactor distance thresholds and buzzer logic

--- Arduino-Code.ino
+++ Arduino-Code.ino
@@ -1,49 +1,55 @@
 const int Trig = 2;
 const int Echo = 3;
 const int Buzzer = 8;
-const int minThreshold = 15;
-const int maxThreshold = 25;
+const int MIN_DISTANCE = 5;
+const int MAX_DISTANCE = 100;
 
-void setup() {
+
+
+void setup() 
+{
   Serial.begin(9600);
-
   pinMode(Trig, OUTPUT);
   pinMode(Echo, INPUT);
   pinMode(Buzzer, OUTPUT);
 }
 
-void loop() {
-
+void loop() 
+{
   long duration;
   int distance;
 
   digitalWrite(Trig, LOW);
   delayMicroseconds(2);
+
   digitalWrite(Trig, HIGH);
   delayMicroseconds(10);
+
   digitalWrite(Trig, LOW);
 
   duration = pulseIn(Echo, HIGH);
 
-  distance = (duration*0.017);
+  distance = duration * 0.017;
 
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
-  
-  if (distance<maxThreshold)and(distance>minThreshold){
+
+  if (distance >= MIN_DISTANCE && distance <= MAX_DISTANCE) 
+  {
+
+    int beepDelay = map(distance, MIN_DISTANCE, MAX_DISTANCE, 50, 600);
+
     digitalWrite(Buzzer, HIGH);
-    delay(100);
+    delay(50);
+
     digitalWrite(Buzzer, LOW);
-    delay(100);
+    delay(beepDelay);
 
   }
-  else  {
-    digitalWrite(Buzzer, HIGH);
-    delay(250);
+  else 
+  {
     digitalWrite(Buzzer, LOW);
-    delay(250);
-
+    delay(100);
   }
-  delay(250);
 }
